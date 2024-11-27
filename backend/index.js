10d48b449412c5625ffe8b064777a4cc449ebe23
priyansh0202-coder <139114935+priyansh0202-coder@users.noreply.github.com>
2024-11-27 17:14:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,25 @@ const bodyParser = require('body-parser')
 
 mongoDB();
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    )
-    next()
-})
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     )
+//     next()
+// })
+
 app.use(bodyParser.json());
-app.use(cors({ origin: "*" }))
+const allowedOrigins = [
+    "http://localhost:3000", 
+    "https://food-delivery-application-frontend-ten.vercel.app"
+];
+app.use(cors({
+             origin: allowedOrigins,
+             methods: ["GET", "POST", "PUT", "DELETE],
+             credentials: true
+             }))
 app.get("/", (req, res) => {
     res.send("hello world");
 });
